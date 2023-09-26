88c06e81bbd40cff2155bf1b7574aa8978c8bfd0
Felipe Sales <soufeliposales@gmail.com>
2023-09-26 20:09:25 -0300

    feat: add basic iOS Home Screen widget

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by Felipe Sales on 26/09/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
