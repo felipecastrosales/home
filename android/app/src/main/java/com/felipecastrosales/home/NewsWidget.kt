4a8dafc95a81d3399e7549bf739fd9f58867d85c
Felipe Sales <soufeliposales@gmail.com>
2023-10-01 12:41:39 -0300

    feat: bump home_widget package and widget

--- android/app/src/main/java/com/felipecastrosales/home/NewsWidget.kt
+++ android/app/src/main/java/com/felipecastrosales/home/NewsWidget.kt
@@ -3,8 +3,10 @@ package com.felipecastrosales.home
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.widget.RemoteViews
-
+import java.io.File
 import es.antonborri.home_widget.HomeWidgetPlugin
 
 /**
@@ -17,14 +19,26 @@ class NewsWidget : AppWidgetProvider() {
         appWidgetIds: IntArray,
     ) {
         for (appWidgetId in appWidgetIds) {
-            // Get reference to SharedPreferences.
             val widgetData = HomeWidgetPlugin.getData(context)
             val views = RemoteViews(context.packageName, R.layout.news_widget).apply {
+
                 val title = widgetData.getString("headline_title", null)
                 setTextViewText(R.id.headline_title, title ?: "No title set")
+
                 val description = widgetData.getString("headline_description", null)
                 setTextViewText(R.id.headline_description, description ?: "No description set")
+
+                val imageName = widgetData.getString("filename", null)
+                val imageFile = File(context.filesDir, imageName ?: "no_image")
+                val imageExists = imageFile.exists()
+                if (imageExists) {
+                    val myBitmap: Bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+                    setImageViewBitmap(R.id.widget_image, myBitmap)
+                } else {
+                    println("image not found!, looked @: ${imageFile.absolutePath}")
+                }
             }
+
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
